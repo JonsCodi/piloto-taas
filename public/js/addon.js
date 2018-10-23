a4765d91722e5c6243bf73c7730e5a559daa39e6
JonsCodi <jonasgoms@hotmail.com>
2018-10-22 21:50:41 -0300

    Melhoras e tentativa de ajustar o problema de CORS

--- public/js/addon.js
+++ public/js/addon.js
@@ -15,22 +15,36 @@ function POST(){
 AJS.$(function(){
 
     var key = [];
-      AJS.$.ajax({
-            type: "GET",
-            url: "https://taasdev.atlassian.net/rest/api/3/search?jql=project=TAS+order+by+key",
-            async: false,
-            headers: {
-              'Accept': 'application/json',
-              "Authorization": "Basic " + btoa("username" + ":" + "password")
-            }
-        }).done(function (data) {
-           console.log(data);
-           for(var i = 0;i<data.issues.length;i++){
-             key.push(data.issues[i].key);
-           }
-        }).error(function (err) {
-           console.log(err);
-        });
+      // AJS.$.ajax({
+      //       type: "GET",
+      //       url: "https://taasdev.atlassian.net/rest/api/3/search?jql=project=TAS+order+by+key",
+      //       async: false,
+      //       headers: {
+      //         'Accept': 'application/json',
+      //         "Authorization": "Basic " + btoa("username" + ":" + "password")
+      //       }
+      //   }).done(function (data) {
+      //      console.log(data);
+      //      for(var i = 0;i<data.issues.length;i++){
+      //        key.push(data.issues[i].key);
+      //      }
+      //   }).error(function (err) {
+      //      console.log(err);
+      //   });
+setTimeout(function(){
+  var urlGetIssuesByProject = "https://taasdev.atlassian.net/rest/api/3/search?jql=project=TAS+order+by+key";
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+        console.log(xhr)
+    }
+  }
+  xhr.open('GET', urlGetIssuesByProject, false);
+  xhr.withCredentials = true
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Authorization", "Basic " + btoa("username" + ":" + "password"))
+  xhr.send(null);
+}, 4000);
 console.log(key)
   for(var i = 0;i<key.length;i++){
     AJS.$.ajax({
@@ -46,8 +60,8 @@ console.log(key)
          if(data.fields.attachment.length > 0){
            const link = data.fields.attachment[0].content;
            const table = '<tr>'+
-                            '<td id="issue-key"><a href="'+key[i]+'">'+key[i]+'</a></td>'+
-                            '<td id="attach"><a href="'+link+'">Resultado do Teste</a></td>'+
+                            '<td id="issue-key"><a href=https://taasdev.atlassian.net/browse/'+key[i]+' target="_blank">'+key[i]+'</a></td>'+
+                            '<td id="attach"><a href='+link+'>Resultado do Teste</a></td>'+
                           '</tr>';
            AJS.$('table').find('tbody').append(table);
          }
